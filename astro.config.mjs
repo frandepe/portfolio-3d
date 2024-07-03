c240a8070eea0c3f34228410c6ebcb299e8e18d8
unknown <frandepaulo23@gmail.com>
2024-07-03 11:50:08 -0300

    netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/edge-functions";
-import vercel from "@astrojs/vercel/serverless";
+// import netlify from '@astrojs/netlify';
+// import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
   // adapter: vercel(),
 });
